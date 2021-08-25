7bd1c220e32657f67023113aa43b37a5dd42653a
Athul George <athulg@qburst.com>
2021-08-25 14:30:10 +0530

    Remove unwanted code

--- TagListView/TagView.swift
+++ TagListView/TagView.swift
@@ -63,7 +63,7 @@ open class TagView: UILabel {
     
     open var textFont: UIFont = .systemFont(ofSize: 12) {
         didSet {
-//            font = textFont
+            font = textFont
         }
     }
 
@@ -78,11 +78,7 @@ open class TagView: UILabel {
     public init(title: NSAttributedString) {
         super.init(frame: CGRect.zero)
 
-//        if (title.isHTML) {
-//            attributedText = title.htmlToAttributedString
-//        } else {
-            attributedText = title
-//        }
+        attributedText = title
         setupView()
     }
     
@@ -116,89 +112,3 @@ private extension UIControl {
     typealias State = UIControlState
 }
 #endif
-
-
-//extension String {
-//    var htmlToAttributedString: NSAttributedString? {
-//        guard let data = data(using: .utf8) else { return nil }
-//        do {
-//            return try NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding:String.Encoding.utf8.rawValue], documentAttributes: nil)
-//        } catch {
-//            return nil
-//        }
-//    }
-//    var htmlToString: String {
-//        return htmlToAttributedString?.string ?? ""
-//    }
-//
-//    var isHTML: Bool {
-//        if isEmpty {
-//            return false
-//        }
-//        return (range(of: "<(\"[^\"]*\"|'[^']*'|[^'\">])*>", options: .regularExpression) != nil)
-//    }
-//}
-//
-//
-//open class LinkedLabel: UILabel {
-//
-//    fileprivate let layoutManager = NSLayoutManager()
-//    fileprivate let textContainer = NSTextContainer(size: CGSize.zero)
-//    fileprivate var textStorage: NSTextStorage?
-//
-//
-//    override init(frame aRect:CGRect){
-//        super.init(frame: aRect)
-//        self.initialize()
-//    }
-//
-//    required public init?(coder aDecoder: NSCoder) {
-//        super.init(coder: aDecoder)
-//        self.initialize()
-//    }
-//
-//    func initialize(){
-//
-//        let tap = UITapGestureRecognizer(target: self, action: #selector(LinkedLabel.handleTapOnLabel))
-//        self.isUserInteractionEnabled = true
-//        self.addGestureRecognizer(tap)
-//    }
-//
-//    open override var attributedText: NSAttributedString?{
-//        didSet{
-//            if let _attributedText = attributedText{
-//                self.textStorage = NSTextStorage(attributedString: _attributedText)
-//
-//                self.layoutManager.addTextContainer(self.textContainer)
-//                self.textStorage?.addLayoutManager(self.layoutManager)
-//
-//                self.textContainer.lineFragmentPadding = 0.0;
-//                self.textContainer.lineBreakMode = self.lineBreakMode;
-//                self.textContainer.maximumNumberOfLines = self.numberOfLines;
-//            }
-//
-//        }
-//    }
-//
-//    @objc func handleTapOnLabel(tapGesture:UITapGestureRecognizer){
-//
-//        let locationOfTouchInLabel = tapGesture.location(in: tapGesture.view)
-//        let labelSize = tapGesture.view?.bounds.size
-//        let textBoundingBox = self.layoutManager.usedRect(for: self.textContainer)
-//        let textContainerOffset = CGPoint(x: ((labelSize?.width)! - textBoundingBox.size.width) * 0.5 - textBoundingBox.origin.x, y: ((labelSize?.height)! - textBoundingBox.size.height) * 0.5 - textBoundingBox.origin.y)
-//
-//        let locationOfTouchInTextContainer = CGPoint(x: locationOfTouchInLabel.x - textContainerOffset.x, y: locationOfTouchInLabel.y - textContainerOffset.y)
-//        let indexOfCharacter = self.layoutManager.characterIndex(for: locationOfTouchInTextContainer, in: self.textContainer, fractionOfDistanceBetweenInsertionPoints: nil)
-//
-//
-//        let rangeX = NSMakeRange(0, self.attributedText?.length ?? 0)
-//        let options = NSAttributedString.EnumerationOptions(rawValue: UInt(0))
-//        self.attributedText?.enumerateAttribute(.link, in: rangeX, options: options, using: { attributes, range, stop in
-//            if NSLocationInRange(indexOfCharacter, range){
-//                if let attr = attributes as? URL {
-//                    UIApplication.shared.open(attr, options: [:], completionHandler: nil)
-//                }
-//            }
-//        })
-//    }
-//}
